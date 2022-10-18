2c5d663728b97242703a643f998c29f15915139d
Vincent Jia <rumble.vecent@outlook.com>
2022-10-18 21:18:27 +0800

    Add ProtobufSupport. Add README and icon to the NuGet package. Update its version to 1.1.1.

---
+++ Prism.Remoting.ProtobufSupport/ProtobufProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Prism.Remoting.ProtobufSupport;
+
+public class ProtobufProvider : ICoderProvider
+{
+    public static void Encode<TData>(TData data, MemoryStream stream)
+    {
+        ProtoBuf.Serializer.Serialize(stream, data);
+    }
+
+    public static TData Decode<TData>(MemoryStream stream)
+        => ProtoBuf.Serializer.Deserialize<TData>(stream);
+    
+    public DataCoder GetEncoder(Type dataType)
+    {
+        return (code, stream) =>
+        {
+            code.Emit(OpCodes.Ldloc, stream);
+            code.Emit(OpCodes.Call, 
+                typeof(ProtobufProvider).
+                    GetMethod(nameof(Encode), 
+                        BindingFlags.Public | BindingFlags.Static)!
+                    .MakeGenericMethod(dataType));
+        };
+    }
+
+    public DataCoder GetDecoder(Type dataType)
+    {
+        return (code, stream) =>
+        {
+            code.Emit(OpCodes.Ldloc, stream);
+            code.Emit(OpCodes.Call, 
+                typeof(ProtobufProvider).
+                    GetMethod(nameof(Decode), 
+                        BindingFlags.Public | BindingFlags.Static)!
+                    .MakeGenericMethod(dataType));
+        };
+    }
+}
