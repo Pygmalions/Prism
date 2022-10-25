8503c3c530b6b49385ad42fdd67943f75b16bf81
Vincent Jia <rumble.vecent@outlook.com>
2022-10-25 10:27:01 +0800

    Add chain search to the injection container.

--- Prism.Injecting/InjectionContainer.cs
+++ Prism.Injecting/InjectionContainer.cs
@@ -3,6 +3,13 @@ namespace Prism.Injecting;
 public class InjectionContainer : IContainer
 {
     private readonly Dictionary<Type, Dictionary<string, object>> _injections = new();
+
+    /// <summary>
+    /// Query chain for this container to use.
+    /// If this container can not find an injection,
+    /// it will try to look up in containers in this chain.
+    /// </summary>
+    public List<InjectionContainer> Chain;
     
     /// <summary>
     /// Get the injection with the specified category and optional ID.
@@ -12,10 +19,12 @@ public class InjectionContainer : IContainer
     /// <returns>Injection object, or null if none matches.</returns>
     public object? Get(Type category, string? id = null)
     {
-        if (!_injections.TryGetValue(category, out var group))
-            return null;
-        if (!group.TryGetValue(id ?? "", out var instance)) 
-            return null;
+        if (!_injections.TryGetValue(category, out var group) ||
+            !group.TryGetValue(id ?? "", out var instance))
+        {
+            return Chain.Select(chainedContainer => chainedContainer.Get(category, id)).
+                FirstOrDefault(result => result != null);
+        }
         var instanceType = instance.GetType();
         if (instanceType.IsGenericType && instanceType.GetGenericTypeDefinition() == typeof(Func<>) 
                                        && instanceType.GetGenericArguments().Length == 1 
